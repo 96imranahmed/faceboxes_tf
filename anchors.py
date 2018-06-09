90c51e281edf163e0b6489566939342400f67bd9
Imran Ahmed <96imranahmed@gmail.com>
2018-06-09 19:28:46 +0100

    Encoding updates

--- anchors.py
+++ anchors.py
@@ -2,6 +2,14 @@ import numpy as np
 import itertools
 import cv2
 
+# Anchor configuration
+CONFIG = [[1024, 1024, 32, 32, 32, 32, 4], 
+          [1024, 1024, 32, 32, 64, 64, 2],
+          [1024, 1024, 32, 32, 128, 128, 1],
+          [1024, 1024, 64, 64, 256, 256, 1],
+          [1024, 1024, 128, 128, 512, 512, 1]] 
+
+
 def densify(cx, cy, scale_x_norm, scale_y_norm, factor):
     box_width_x = scale_x_norm + (factor - 1)*scale_x_norm/factor 
     box_width_y = scale_y_norm + (factor - 1)*scale_y_norm/factor
@@ -16,6 +24,17 @@ def densify(cx, cy, scale_x_norm, scale_y_norm, factor):
         anchor_dense[w, h] = np.array([x, y, x + scale_x_norm, y + scale_y_norm])
     return np.reshape(anchor_dense, (factor**2, 4))
 
+def get_boxes(input_config):
+    b_out = []
+    b_std = []
+    shape_stub = []
+    for lst in input_config:
+        _, b_cur = get_anchor_boxes(lst[0], lst[1], lst[2], lst[3], lst[4], lst[5], lst[6])
+        b_std.append(b_cur)
+        shape_stub.append(b_cur.shape[:-1])
+        b_out.append(np.reshape(b_cur, (-1, 4)).copy())
+    return np.vstack(tuple(b_out)), b_std, shape_stub
+    
 def get_anchor_boxes(shape_dim_x, shape_dim_y, space_x, space_y, scale_x, scale_y, densify_rate):
     # Returns boxes in (left, top, bottom right) form (same as wider) - (0, 0) top left
     # Coordinates stay consistent with OpenCV
@@ -51,4 +70,7 @@ def transform_ltbr_to_lbwh(box):
     height = np.abs(box[3] - box[1])
     c_p = [box[0], box[1] + height, width, -1*height]
     return c_p
-# coords, boxes = get_anchor_boxes(1024, 1024, 32, 32, 32, 32)
+
+
+
+boxes_vec, boxes_lst, stubs = get_boxes(CONFIG)
